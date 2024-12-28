7615bda7fdeb3c6b2364ba6ab8de30dab053902e
Babatunde <tundeloper@gmail.com>
2024-12-28 23:52:58 +0100

    password line and eye visisble icon to check visibity of the text

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
 import React, { useState } from "react"
+import { Box, Button, IconButton, InputAdornment, TextField, Typography } from "@mui/material"
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import CountriesDropDown from "../components/contrydropdown"
 import FavSport from "../components/FavSport"
 import FavTeam from "../components/FavTeam"
@@ -17,10 +18,15 @@ import axios from "axios"
 //     password:string,
 //   }
 
-interface formprps {name: string, email: string,  password?: string, dateOfBirth: string, country: SingleValue<{label: string, value: string,}>, favSport: SingleValue<{label: string, value: string,}>, FavSportTeam: SingleValue<{label: string, value: string,}>}
+interface formprps { name: string, email: string, password?: string, dateOfBirth: string, country: SingleValue<{ label: string, value: string, }>, favSport: SingleValue<{ label: string, value: string, }>, FavSportTeam: SingleValue<{ label: string, value: string, }> }
 
-const SignUp: React.FC<{visible: boolean}> = ({visible}) => {
-    const [userData, setUserData] = useState<formprps>({name: '', email: '', dateOfBirth: '2014-12-14', country: {label: '', value: ''}, favSport: {label: '', value: ''}, FavSportTeam: {label: '', value: ''}})
+const SignUp: React.FC<{ visible: boolean }> = ({ visible }) => {
+    const [userData, setUserData] = useState<formprps>({ name: '', email: '', dateOfBirth: '2014-12-14', country: { label: '', value: '' }, favSport: { label: '', value: '' }, FavSportTeam: { label: '', value: '' } })
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+    };
     // const [error, setError] = useState<string | null>(null)
 
     const userCredentials = {
@@ -36,10 +42,10 @@ const SignUp: React.FC<{visible: boolean}> = ({visible}) => {
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setUserData((prev) => ({
-          ...prev,
-          [name]: value,
+            ...prev,
+            [name]: value,
         }));
-      };
+    };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -48,7 +54,7 @@ const SignUp: React.FC<{visible: boolean}> = ({visible}) => {
             const response = await axios.post("https://lazeapi-2.onrender.com/signup/", userCredentials)
             console.log(response)
         } catch (error) {
-            if(axios.isAxiosError(error)) {
+            if (axios.isAxiosError(error)) {
                 console.log(error.status)
                 console.log(error.message)
             }
@@ -56,12 +62,25 @@ const SignUp: React.FC<{visible: boolean}> = ({visible}) => {
     }
 
 
-    return <div className={`flex hidden flex-col p-[2rem] justify-center items-center text-black rounded-[1rem] bg-[white] w-[23rem] scale ${visible ? 'reset-position' : 'scale-down'}`} style={{position: "absolute", marginTop: '-3rem'}}>
+    return <div className={`flex hidden flex-col p-[2rem] justify-center items-center text-black rounded-[1rem] bg-[white] w-[23rem] scale ${visible ? 'reset-position' : 'scale-down'}`} style={{ position: "absolute", marginTop: '-3rem' }}>
         <Box component="form" className="w-full" onSubmit={handleSubmit}>
             {/* <input placeholder="Name" style={{outline: 'none'}} className="mb-2" /> */}
-            <TextField fullWidth placeholder="Name" style={{marginBottom: '.5rem'}} name="name" value={userData.name} onChange={handleInputChange}/>
-            <TextField fullWidth placeholder="Email" style={{marginBottom: '.5rem'}} name="email" value={userData.email} onChange={handleInputChange}/>
-            <TextField fullWidth placeholder="Password" style={{marginBottom: '.5rem'}} name="password" onChange={handleInputChange}/>
+            <TextField fullWidth placeholder="Name" style={{ marginBottom: '.5rem' }} name="name" value={userData.name} onChange={handleInputChange} />
+            <TextField fullWidth placeholder="Email" style={{ marginBottom: '.5rem' }} name="email" value={userData.email} onChange={handleInputChange} />
+            <TextField
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                fullWidth
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton onClick={togglePasswordVisibility} edge="end">
+                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
             {/* <TextField fullWidth placeholder="Password" style={{marginBottom: '.5rem'}}/> */}
             <Typography gutterBottom >Date of Birth</Typography>
             <DOB setUserData={setUserData} userData={userData} />
@@ -73,12 +92,12 @@ const SignUp: React.FC<{visible: boolean}> = ({visible}) => {
                 <FavSport setUserData={setUserData} userData={userData} />
             </div>
             <div className="mb-4">
-                <FavTeam setUserData={setUserData} userData={userData}/>
+                <FavTeam setUserData={setUserData} userData={userData} />
             </div>
             {/* <TextField fullWidth placeholder="Favourite Sport" style={{marginBottom: '.5rem'}}/>
             <TextField fullWidth placeholder="Favourite Sport Team"/> */}
             <div className="flex justify-center w-full">
-            <Button sx={{color: 'white', background: '#463a85', borderRadius: '2rem', textTransform: 'capitalize', padding: '10px 2.5rem',}} type="submit">Create Account</Button>
+                <Button sx={{ color: 'white', background: '#463a85', borderRadius: '2rem', textTransform: 'capitalize', padding: '10px 2.5rem', }} type="submit">Create Account</Button>
             </div>
         </Box>
     </div>
