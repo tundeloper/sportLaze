00453eae304531120386c2bd846eb0cee8248cf9
Babatunde <tundeloper@gmail.com>
2025-02-02 16:38:48 +0100

    new client id

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,19 @@ const Lounge = lazy(() => import('./pages/lounge'))
 function App() {
   return (
     <SportlazeProvider>
-      <GoogleOAuthProvider clientId='1068447635655-46m9unr93bpd6hgpsoar73kpkvrtot83.apps.googleusercontent.com'>
+      <GoogleOAuthProvider clientId='292887638276-kk8gmqfsjivcnjujhsiqiu5d62rkocqt.apps.googleusercontent.com'>
       <div className="App">
         <Routes>
           <Route
-            path='/' element={<Suspense fallback={<Loading />}><Home /></Suspense>} />
+            path='/' element={<Suspense fallback={<Loading />}><Protect redirectPath='auth'><Home /></Protect></Suspense>} />
           <Route
             path='/auth' element={<Suspense fallback={<div>Loading...</div>}><Welcome /></Suspense>} />
           <Route
-            path='/lounge' element={<Suspense fallback={<div>Loading...</div>}><Lounge /></Suspense>} />
+            path='/lounge' element={<Suspense fallback={<div>Loading...</div>}><Protect redirectPath='auth'><Lounge /></Protect></Suspense>} />
           <Route
             path='/videos' element={<Suspense fallback={<div>Loading...</div>}><Protect redirectPath='auth'><div>Videos Route</div></ Protect></Suspense>} />
           <Route
-            path='/lounge/:lounge' element={<Suspense fallback={<div>Loading...</div>}><LoungeId /></Suspense>} />
+            path='/lounge/:lounge' element={<Suspense fallback={<div>Loading...</div>}><Protect redirectPath='auth'><LoungeId /></Protect></Suspense>} />
           <Route
             path='*' element={<Suspense fallback={<div>Loading...</div>}><div>Invalid Route</div></Suspense>} />
         </Routes>
