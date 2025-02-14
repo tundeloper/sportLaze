a3643a9a86b23323928e30e3cd9a90203684db06
Babatunde <tundeloper@gmail.com>
2025-02-14 10:08:31 +0100

    making the fullwidth a button

--- src/utils/googleLoginBtn.tsx
+++ src/utils/googleLoginBtn.tsx
@@ -84,15 +84,12 @@ const GoogleLoginButton = () => {
   };
 
   return (
-    <div className="w-full flex justify-center">
-      <div
-        onClick={loginHandler}
-        className="flex items-center justify-center gap-2 bg-white border border-gray-300 shadow-md py-2 rounded-full font-bold text-black hover:bg-gray-100 transition duration-300 w-full max-w-[400px] cursor-pointer"
-      >
+    <button className="w-full flex justify-center" onClick={loginHandler}>
+      <div className="flex items-center justify-center gap-2 bg-white border border-gray-300 shadow-md py-2 rounded-full font-bold text-black hover:bg-gray-100 transition duration-300 w-full max-w-[400px] cursor-pointer">
         <GoogleIcon />
         <span className="font-bold">Sign up with Google</span>
       </div>
-    </div>
+    </button>
   );
 };
 
